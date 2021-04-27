742b12eadcae9e684f280d0dff8616660b8908f3
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-27 15:52:35 -0700

    Implement mongoose

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(){
+  console.log('mongoose is connected');
+});
+
 app.get('/ping', (request, response) => {
   response.send('pong');
 });
