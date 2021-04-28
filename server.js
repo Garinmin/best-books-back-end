5bd59300c70affb8e22a4db067518b4d8c84723f
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-28 12:42:23 -0700

    Add POST and DELETE handlers for /books route

--- server.js
+++ server.js
@@ -35,17 +35,61 @@ const UserModel = require('./models/User');
 // Rinat.save();
 
 async function getAllUsers(request, response) {
-  const { email } = request.query.email;
+  const email = request.query.email;
   console.log({email});
 
-  await UserModel.find(email, (err, users) => {
+  await UserModel.find({email}, (err, users) => {
+    console.log({users});
     response.send(users[0].books);
   });
-
 }
 
 app.get('/books', getAllUsers);
 
+app.post('/books', async (request, response) => {
+  const books = request.body;
+  const email = request.query.email;
+
+  await UserModel.find({email}, (err, users) =>{
+    if(users.length) {
+      const currentUser = users[0];
+      const currentBooks = currentUser.books;
+
+      currentBooks.push(books);
+
+      currentUser.save();
+
+      response.send(currentBooks);
+    } else {
+      response.send('no user found');
+    }
+  });
+});
+
+app.delete('/books/:index', async (request, response) => {
+  const email = request.query.email;
+
+  const index = Number(request.params.index);
+
+  console.log({index});
+  console.log({email});
+
+  await UserModel.find({email}, (err, users) => {
+
+    if (users.length) {
+      const user = users[0];
+
+      const currentBooks = user.books;
+
+      currentBooks.splice(index, 1);
+
+      user.save();
+      response.send(currentBooks);
+    } else {
+      response.send('no user found');
+    }
+  });
+});
 
 
 app.get('/ping', (request, response) => {
